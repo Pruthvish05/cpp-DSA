796364093658dd4112a48ddf68be2f2d0bfb56aa
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2025-04-01 23:03:37 +0530

    Update Loops1.cpp

--- Loops1.cpp
+++ Loops1.cpp
@@ -12,14 +12,4 @@ int main() //{
 	    }
 	    cout<<"\n";
 	}
-	int n;
-	cout<<"Enter num";
-	cin>>n;
-	for(int i=1;i<=n;i++)
-	{
-	    for(char ch=65;ch<=(64+n);ch++)
-	    {
-	        cout<<ch<<" ";
-	    }
-	    cout<<"\n";
-	}
+	
