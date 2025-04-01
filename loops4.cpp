a2ab10a236673581abd5c4fa1176798274b311f7
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2025-04-01 23:07:25 +0530

    Create loops4.cpp

---
+++ loops4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int n;
+    cout<<"Enter n";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i+1;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    another one
+    int n;
+    cout<<"enter n";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<i+1<<" ";
+        }
+        cout<<endl;
+    }
