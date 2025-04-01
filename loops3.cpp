2a8e6af5147a212d39f98e867b1b8a7f3741ff99
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2025-04-01 23:05:45 +0530

    Update loops3.cpp

--- loops3.cpp
+++ loops3.cpp
@@ -11,3 +11,19 @@ int n;
 	    }
 	    cout<<"\n";
 	}
+//Another one
+#include<iostream>
+using namespace std;
+int n;
+	cout<<"Enter num";
+	cin>>n;
+    char ch='A';
+	for(int i=65; i<(65+n); i++) {
+		for(char j='A'; j<'A'+n; j++) {
+		
+			cout<<ch<<" ";
+			ch++;
+		}
+		cout<<endl;
+	}
+}
