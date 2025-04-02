23d595efb5203ecc9a64ddd4879ba634f0dc88f7
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2025-04-02 22:40:34 +0530

    Update loops4.cpp

--- loops4.cpp
+++ loops4.cpp
@@ -11,13 +11,34 @@ int n;
         }
         cout<<endl;
     }
-    another one
+    //another one
     int n;
     cout<<"enter n";
     cin>>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<i+1;j++){
             cout<<i+1<<" ";
+        }
+        cout<<endl;
+    }
+//one more
+ int n;
+    cout<<"enter n";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    int n;
+    cout<<"enter n";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j>=0;j--)
+        {
+            cout<<j<<" ";
         }
         cout<<endl;
     }
