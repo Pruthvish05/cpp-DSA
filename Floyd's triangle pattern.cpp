9540c53205bbce9f3798d98908f36a25c9edc80f
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2025-04-02 22:41:11 +0530

    Create Floyd's triangle pattern.cpp

---
+++ Floyd's triangle pattern.cpp
@@ -0,0 +1,15 @@
+ floyds triangle pattern.
+      int n;
+      int num=1;
+    cout<<"enter n";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i+1;j++)
+        {
+           
+            cout<<num;
+            num++;
+        }
+        cout<<endl;
+    }
